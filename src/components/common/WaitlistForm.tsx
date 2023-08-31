f3375a5603df36fb045cb0dcbcb80f83cc16e814
Ramez <ramez@pursue.today>
2023-08-31 15:20:49 +0500

    Added some Page Insights fixes 2.0

--- src/components/common/WaitlistForm.tsx
+++ src/components/common/WaitlistForm.tsx
@@ -28,7 +28,7 @@ const WaitlistForm = () => {
                 {...field}
                 type="text"
                 placeholder="First Name"
-                className="w-full border border-[#B4BCC3] rounded-[12px] p-3 px-6 text-[#919EAB]"
+                className="w-full border border-[#B4BCC3] rounded-[12px] p-3 px-6 text-[#333]"
               />
             )}
           />
@@ -44,7 +44,7 @@ const WaitlistForm = () => {
                 {...field}
                 type="text"
                 placeholder="Last Name"
-                className="w-full border border-[#B4BCC3] rounded-[12px] p-3 px-6 text-[#919EAB]"
+                className="w-full border border-[#B4BCC3] rounded-[12px] p-3 px-6 text-[#333]"
               />
             )}
           />
@@ -61,7 +61,7 @@ const WaitlistForm = () => {
                 {...field}
                 type="text"
                 placeholder="Last Name"
-                className="w-full border border-[#B4BCC3] rounded-[12px] p-3 px-6 text-[#919EAB]"
+                className="w-full border border-[#B4BCC3] rounded-[12px] p-3 px-6 text-[#333]"
               />
             )}
           />
--- src/components/pricing/PricingCard.tsx
+++ src/components/pricing/PricingCard.tsx
@@ -6,7 +6,6 @@ import { Icons } from "../ui/Icons";
 import OutlineButton from "../ui/OutlineButton";
 import SolidButton from "../ui/SolidButton";
 import Container from "@/layouts/container";
-import PircingDiamond from '/public/images/pricing_diamon.svg';
 import PircingDiamondComplete from '/public/images/pricing_diamond_complete.svg';
 import PircingDarts from '/public/images/pricing_darts.svg';
 
@@ -88,27 +87,23 @@ export default function PricingCard({ isMonthly, isHome=false } : {
                     "mt-8 space-y-3 text-sm leading-6"
                   )}
                 >
-                  {plan.features &&
-                    plan.features.map((feature) => (
-                      <ul 
-                        key={feature.id}
-                        className="flex items-center gap-x-3 font-bold"
+                  {plan.features && plan.features.map((feature) => (
+                    <div key={feature.id} className="flex items-center gap-x-3 font-bold">
+                      <span>
+                        {feature.enabled ? (
+                          <Icons.checkCircle className={plan.mostPopular ? "text-white font-bold" : "text-teal-500"}/>
+                        ) : null}
+                      </span>
+                      <span
+                        className={cn(
+                          feature.enabled ? (plan.mostPopular ? "text-white" : "text-[#343434]") : "text-[#919EAB]",
+                          "flex gap-x-3"
+                        )}
                       >
-                        {" "}
-                        <span>
-                          {feature.enabled ? <Icons.checkCircle className={plan.mostPopular ? "text-white font-bold" : "text-teal-500"}/> : null}
-                        </span>
-                        <li
-                          key={feature.id}
-                          className={cn(
-                            feature.enabled ? plan.mostPopular ? "text-white" : "text-[#343434]" : "text-[#919EAB]",
-                            'flex gap-x-3'  
-                          )}
-                        >
-                          {feature.text}
-                        </li>
-                      </ul>
-                    ))}
+                        {feature.text}
+                      </span>
+                    </div>
+                  ))}
                 </div>
                 <Link href={plan.href} className="flex justify-center items-center mt-6 py-2 px-1">
                   {plan.mostPopular ? (
